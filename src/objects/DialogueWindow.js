6523652bedf2be477d8e8dd16cb3ef3aee70d7d4
drcaramelsyrup <dr.caramel.syrup@gmail.com>
2017-04-18 23:40:48 -0700

    Scrolling mostly working. Buttons with masks have issues.

--- src/objects/DialogueWindow.js
+++ src/objects/DialogueWindow.js
@@ -22,6 +22,8 @@ function DialogueWindow(game, convoManager/*, ...args*/) {
   // - buttons
 
   this.convoManager = convoManager;
+  // messy, but useful if we need a reference to the game
+  this._game = game;
 
   // private members specifying margin and padding
   this._dialogTextOriginX = 12;
@@ -47,23 +49,28 @@ function DialogueWindow(game, convoManager/*, ...args*/) {
   this.dialogPanel.add(
     this.speakerText = new SlickUI.Element.Text(10, 0, 'Speaker')).centerHorizontally().text.alpha = 0.5;
   
-  var scrollMask = game.make.graphics(0, 0);
-  scrollMask.beginFill(0xffffff);
-  scrollMask.drawRect( this._dialogTextOriginX, this._dialogTextOriginY, this._dialogTextWidth, this._dialogTextHeight );
-  scrollMask.endFill();
+  // using a mask for scrolling purposes
+  this._scrollMask = game.make.graphics(0, 0);
+  this._scrollMask.beginFill(0xffffff);
+  this._scrollMask.drawRect( this._dialogTextOriginX, this._dialogTextOriginY, this._dialogTextWidth, this._dialogTextHeight );
+  this._scrollMask.endFill();
 
-  // this.dialogue
   var style = { font: '14px Arial', fill: '#000000', wordWrap: true, wordWrapWidth: this._dialogTextWidth, align: 'left' };
   this.dialogPanel.add(
     this.dialogText = new SlickUI.Element.DisplayObject(this._dialogTextOriginX, this._dialogTextOriginY, game.make.text(0, 0, 'placeholder text', style)));
 
-  this.dialogPanel.add(new SlickUI.Element.DisplayObject(0, 0, scrollMask));
-  this.dialogText.displayObject.mask = scrollMask;
+  this.dialogPanel.add(new SlickUI.Element.DisplayObject(0, 0, this._scrollMask));
+  this.dialogText.displayObject.mask = this._scrollMask;
 
   this.dialogPanel.alpha = 0.8;
 
   // for removing player choice buttons
   this.buttons = [];
+  // slider for scrolling overflow
+  this.slider = null;
+  // stores each button Y value
+  // also keeps track of how low our content goes; last element is content bottom
+  this._buttonsY = [];
 
 }
 
@@ -76,12 +83,14 @@ DialogueWindow.prototype.begin = function(game, jsonKey) {
 };
 
 DialogueWindow.prototype.display = function() {
-  this.removeButtons();
+  this.cleanWindow();
   this.displayText();
-  // this.displayResponses();
+  this.displayResponses();
+  this.addOverflowScroll();
 };
 
-DialogueWindow.prototype.removeButtons = function () {
+DialogueWindow.prototype.cleanWindow = function () {
+  // remove all buttons
   for (var i = 0; i < this.buttons.length; i++) {
     var button = this.buttons[i];
     button.container.displayGroup.removeAll(true);
@@ -91,38 +100,42 @@ DialogueWindow.prototype.removeButtons = function () {
     button.sprite = undefined;
   }
   this.buttons = [];
+  this._buttonsY = [];
+
+  // remove scroller and restore dialog text position
+  if (this.slider !== null) {
+    this.slider.displayGroup.removeAll(true);
+  }
+  this.dialogText.y = this._dialogTextOriginY;
 };
 
-DialogueWindow.prototype.displayText = function () {
-  // this.dialogText.value = this.convoManager.getCurrentText();
-  
+DialogueWindow.prototype.displayText = function () {  
   this.dialogText.displayObject.text = this.convoManager.getCurrentText();
-  if (this.dialogText.displayObject.getBounds().height > this.dialogHeight - 50) {
-    var slider = new SlickUI.Element.Slider(
-      this._dialogTextWidth + this._dialogPadding, this._dialogPadding, 
-      this._dialogTextHeight, 1 /* scrollbar starts at top, or 1; bottom is 0 */, true /* vertical */);
-    this.dialogPanel.add(slider);
-
-    var heightDiff = this.dialogText.displayObject.getBounds().height - (this.dialogHeight - 50);
-    slider.onDrag.add(function (value) {
-      // mapping height differences to scroll values
-      this.dialogText.y = this._dialogTextOriginY - heightDiff*(1-value);
-    }, this);
-  }
 };
 
 DialogueWindow.prototype.displayResponses = function () {
-  // pixel width
-  var textHeight = this.dialogText.size*1.3;
-  // round up number of lines
-  var lines = Math.ceil(this.dialogText.value.length * this.dialogText.size / this.dialogWidth);
-  var textBottom = this.dialogText.y + lines * textHeight;
-
   // start rendering buttons at the bottom of dialogue
   var responses = this.convoManager.getResponses();
+
+  var textBottom = this._dialogTextOriginY + this.dialogText.displayObject.getBounds().height;
+  var nextButtonY = textBottom;
   for (var i = 0; i < responses.length; i++) {
+    // display text
+    var buttonTextStyle = { font: '14px Arial', fill: '#000000', wordWrap: true, wordWrapWidth: this._dialogTextWidth, align: 'left' };
+    var responseText = this._game.make.text(0, 0, responses[i]['text'], buttonTextStyle);
+    var buttonText = new SlickUI.Element.DisplayObject(
+      Math.round(this.dialogWidth / 2 - responseText.width / 2),0, /* center text */
+      responseText);
+
+    // add to sized button
     var choiceButton;
-    this.dialogPanel.add(choiceButton = new SlickUI.Element.Button(0,textBottom + i*this.dialogText.size*1.5, this.dialogWidth, 24));
+    this.dialogPanel.add(choiceButton = new SlickUI.Element.Button(
+      0,nextButtonY, 
+      this.dialogWidth, responseText.height));
+    choiceButton.add(buttonText);
+    nextButtonY += responseText.height;
+    // useful for overflow scrolling
+    this._buttonsY.push(choiceButton.y);
 
     var responseTarget = responses[i]['target'];
     choiceButton.events.onInputUp.add(
@@ -130,11 +143,36 @@ DialogueWindow.prototype.displayResponses = function () {
         this.dialogueWindow.convoManager.idx = this.responseTarget;
         this.dialogueWindow.display();
       }, {dialogueWindow: this, responseTarget: responseTarget});
-
-    var buttonText = new SlickUI.Element.Text(0,0, responses[i]['text']);
-    buttonText.size = this.dialogText.size;
-    choiceButton.add(buttonText).center();
+    // add mask
+    choiceButton.sprite.mask = this._scrollMask;
+    
+    // keep track of buttons to be deleted
     this.buttons.push(choiceButton);
+  }
+
+  // last element is bottom of content
+  this._buttonsY.push(nextButtonY);
+};
+
+DialogueWindow.prototype.addOverflowScroll = function () {
+  // can we fit everything in the current window?
+  var heightDiff = this._buttonsY[this._buttonsY.length - 1] - this._dialogTextHeight;
+  // add a slider otherwise
+  if (heightDiff > 0) {
+    this.slider = new SlickUI.Element.Slider(
+      this._dialogTextWidth + this._dialogPadding, this._dialogPadding, 
+      this._dialogTextHeight, 1 /* scrollbar starts at top, or 1; bottom is 0 */, true /* vertical */);
+    this.dialogPanel.add(this.slider);
+
+    this.slider.onDrag.add(function (value) {
+      // mapping height differences to scroll values
+      var scrollValue = heightDiff*(1-value);
+      this.dialogText.y = this._dialogTextOriginY - scrollValue;
+      for (var i = 0; i < this.buttons.length; i++) {
+        // slide all buttons up
+        this.buttons[i].y = this.dialogPanel.y + this._buttonsY[i] - scrollValue;
+      }
+    }, this);
   }
 };
 
