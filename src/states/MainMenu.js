68151270c54619dbcff9bf1dca56b9085b99f4e2
Amber Thomas <amberthomas@DN51utbm.SUNet>
2017-05-15 20:04:15 -0700

    pretty main menu page with better font dealing

--- src/states/MainMenu.js
+++ src/states/MainMenu.js
@@ -19,22 +19,25 @@ exports.create = function (game) {
   logo.anchor.setTo(0.5, 0.5);
   logo.alpha = .1;
 
+  let screenFont = "Cinzel Decorative";
+  let fillColor = "#42dff4";
+
 var titleText = game.add.text(game.world.centerX, game.world.centerY- 75, "Quasaria", {
-      font: "75px Cinzel Decorative",
+      font: "75px " + screenFont,
       fontWeight: "bold",
-      fill: "#42dff4",
+      fill: fillColor,
       align: "center"
   });
 
   var contText = game.add.text(game.world.centerX, game.world.centerY, "Continue", {
-        font: "50px Cinzel Decorative",
-        fill: "#42dff4",
+        font: "50px " + screenFont,
+        fill: fillColor,
         align: "center"
     });
 
     var newGameText = game.add.text(game.world.centerX, game.world.centerY + 55, "New Game", {
-          font: "50px Cinzel Decorative",
-          fill: "#42dff4",
+          font: "50px " + screenFont,
+          fill: fillColor,
           align: "center"
       });
 
